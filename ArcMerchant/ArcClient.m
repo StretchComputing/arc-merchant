ddac22812082f54930b503a73a0b26ffcca2498e
Nick Wroblewski <njw438@gmail.com>
2013-06-24 12:07:25 -0500

    update push token

--- ArcMerchant/ArcClient.m
+++ ArcMerchant/ArcClient.m
@@ -409,52 +409,55 @@ NSString *const ARC_ERROR_MSG = @"Arc Error, try again later";
 -(void)updatePushToken{
     
     @try {
-        api = UpdatePushToken;
-        
         [rSkybox addEventToSession:@"updatePushToken"];
+        api = UpdatePushToken;
         
         NSMutableDictionary *pairs = [NSMutableDictionary dictionary];
         
         AppDelegate *mainDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
         
-        [pairs setValue:mainDelegate.pushToken forKey:@"DeviceId"];
-        [pairs setValue:@"Production" forKey:@"PushType"];
+        NSLog(@"Token: %@", mainDelegate.pushToken);
         
-        
+        if ([mainDelegate.pushToken length] > 0) {
+            
+            [pairs setValue:mainDelegate.pushToken forKey:@"DeviceId"];
+            
+            [pairs setValue:@"Production" forKey:@"PushType"];
+            
 #if DEBUG==1
-        [pairs setValue:@"Development" forKey:@"PushType"];
+            [pairs setValue:@"Development" forKey:@"PushType"];
 #endif
+            
+            NSNumber *noMail = [NSNumber numberWithBool:YES];
+            [pairs setValue:noMail forKey:@"NoMail"];
+            
+            NSString *requestString = [NSString stringWithFormat:@"%@", [pairs JSONRepresentation], nil];
+            NSData *requestData = [NSData dataWithBytes: [requestString UTF8String] length: [requestString length]];
+            
+            ArcClient *tmp = [[ArcClient alloc] init];
+            NSString *arcUrl = [tmp getCurrentUrl];
+            
+            NSString *merchantId = [[NSUserDefaults standardUserDefaults] valueForKey:@"merchantId"];
+            merchantId = @"current";
+            
+            NSString *createReviewUrl = [NSString stringWithFormat:@"%@customers/update/%@", arcUrl, merchantId, nil];
+            NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL: [NSURL URLWithString:createReviewUrl]];
+            [request setHTTPMethod: @"POST"];
+            
+            [request setHTTPBody: requestData];
+            [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+            [request setValue:[tmp authHeader] forHTTPHeaderField:@"Authorization"];
+            
+            NSLog(@"Request String: %@", requestString);
+            
+            self.serverData = [NSMutableData data];
+            [rSkybox startThreshold:@"updatePushToken"];
+            self.urlConnection = [[NSURLConnection alloc] initWithRequest:request delegate:self startImmediately: YES];
+            
+            
+        }
         
         
-        NSString *merchantId = [[NSUserDefaults standardUserDefaults] valueForKey:@"merchantId"];
-        [pairs setValue:merchantId forKey:@"merchantId"];
-        
-        NSNumber *noMail = [NSNumber numberWithBool:YES];
-        [pairs setValue:noMail forKey:@"NoMail"];
-        
-        
-        NSString *requestString = [NSString stringWithFormat:@"%@", [pairs JSONRepresentation], nil];
-        NSData *requestData = [NSData dataWithBytes: [requestString UTF8String] length: [requestString length]];
-        
-        ArcClient *tmp = [[ArcClient alloc] init];
-        NSString *arcUrl = [tmp getCurrentUrl];
-        
-        
-        
-        
-        NSString *createReviewUrl = [NSString stringWithFormat:@"%@merchants/managers/update/current/", arcUrl, nil];
-        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL: [NSURL URLWithString:createReviewUrl]];
-        [request setHTTPMethod: @"POST"];
-        
-        [request setHTTPBody: requestData];
-        [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-        [request setValue:[tmp authHeader] forHTTPHeaderField:@"Authorization"];
-        
-        NSString *authHeader = [tmp authHeader];
-        
-        self.serverData = [NSMutableData data];
-        [rSkybox startThreshold:@"updatePushToken"];
-        //NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request delegate:self startImmediately: YES];
     }
     @catch (NSException *e) {
         [rSkybox sendClientLog:@"ArcClient.updatePushToken" logMessage:@"Exception Caught" logLevel:@"error" exception:e];
